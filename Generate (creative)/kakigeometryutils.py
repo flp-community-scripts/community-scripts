1a7c4e3180a9430c1c51132544cebcf19b8fdaf5
Olivier Stuker <oli4stuker@hotmail.com>
2024-04-29 10:51:32 +0200

    ADDED getPointsPlane and getFigurePlane geometry utils

--- Generate (creative)/kakigeometryutils.py
+++ Generate (creative)/kakigeometryutils.py
@@ -3,7 +3,7 @@ Copyright 2024 Olivier Stuker a.k.a. BinaryBorn
 """
 
 from kakiprimitives import vec4, mat4, box, figure
-from kakiutils import getBoundingBox, transform
+from kakiutils import getBoundingBox, transform, normvec, dotprod, crossprod
 
 def getFigureBoundingBox(figure: figure, round: bool = False) -> box:
   """Returns the bounding box of a whole figure.
@@ -12,6 +12,55 @@ def getFigureBoundingBox(figure: figure, round: bool = False) -> box:
   for pts in figure:
     points.extend(pts)
   return getBoundingBox(points, round)
+
+def getPointsPlane(points: list[vec4]) -> vec4 | None:
+  """Returns the plane equation for a list of points
+  (assuming they're planar).
+  The plane equation is nx*x + ny*y + nz*z = d
+
+  Returns:
+    vec4: x,y,z = plane normal (nx, ny, nz), w = scalar component (d)
+  """
+  i = 0
+  pA: vec4
+  pAB: vec4
+  pB: vec4
+  pC: vec4
+  pBC: vec4
+  for point in points:
+    if i == 0:
+      pA = point
+      i += 1
+    elif i == 1:
+      pB = point
+      pAB = vec4(pB.x - pA.x, pB.y - pA.y, pB.z - pA.z)
+      i += 1
+    else:
+      pC = point
+      pBC = vec4(pC.x - pB.x, pC.y - pB.y, pC.z - pB.z)
+      cross = crossprod(pAB, pBC)
+      norm = normvec(cross)
+      # if that product has a length, we're done
+      if norm > 0:
+        # normalize cross product to make it a norm vector
+        cross.x /= norm
+        cross.y /= norm
+        cross.z /= norm
+        cross.w = dotprod(cross, pA)
+        return cross
+
+def getFigurePlane(figure: figure) -> vec4 | None:
+  """Returns the plane equation for a figure
+  (assuming it's planar).
+  The plane equation is nx*x + ny*y + nz*z = d
+
+  Returns:
+    vec4: x,y,z = plane normal (nx, ny, nz), w = scalar component (d)
+  """
+  for polygon in figure:
+    norm = getPointsPlane(polygon)
+    if norm is not None:
+      return norm
 
 def transformPoints(points: list[vec4], t: mat4) -> None:
   """Transforms all points in list in-place.
