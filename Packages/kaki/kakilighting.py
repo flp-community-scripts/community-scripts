e7a34a9485e2cee92a7ec0542f9600fe66f5b973
Olivier Stuker <oli4stuker@hotmail.com>
2024-05-03 15:41:50 +0200

    ADDED shininess parameter to lighting

--- Packages/kaki/kakilighting.py
+++ Packages/kaki/kakilighting.py
@@ -6,7 +6,7 @@ from kakigeometryutils import getPointsPlane
 from kakiprimitives import vec4, phenotype, mesh
 from kakiutils import vecnorm, normalize, dotprod, interpolatePhenotypes
 
-def lightMesh(mesh: mesh, lightVector: vec4, lightPheno: phenotype):
+def lightMesh(mesh: mesh, lightVector: vec4, lightPheno: phenotype, shininess: float = 0.0):
   """Lights a mesh in place.
   """
   # invert the light vector, because face normals need to be aligned
@@ -18,5 +18,7 @@ def lightMesh(mesh: mesh, lightVector: vec4, lightPheno: phenotype):
     norm = vecnorm(mesh.normals[i])
     amt = 0 if norm == 0 else dotprod(mesh.normals[i], lightVector) / norm
     amt = min(max(amt, 0), 1)
+    if shininess > 0:
+      amt = (pow(2, 10 * shininess * amt) - 1) / (pow(2, 10 * shininess) - 1)
     # mix material phenotype with light
     mesh.phenos[i] = interpolatePhenotypes([mesh.phenos[i], lightPheno], [1 - amt, amt])
